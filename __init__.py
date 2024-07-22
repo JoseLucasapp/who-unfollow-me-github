610d629dad94c96d5b95f5dc364bb6e34b505524
José Lucas <josluc2016@gmail.com>
2024-07-22 00:51:21 -0300

    fix: set user by input

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 from bot.index import App
 
 if __name__ == '__main__':
-    user = 'joselucasapp'
-    base_url = f'https://github.com/{user}'
+    github_username = input(
+        "Please enter your GitHub username (without the '@'): ")
+    base_url = f'https://github.com/{github_username}'
     bot = App(base_url=base_url)
     bot.next_button('followers')
     bot.next_button('followings')
